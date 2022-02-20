fdb7ee0e7facfbd79212372ed67ffd36987b883c
Danny Stewart <danny@stewart.cc>
2022-02-20 16:37:22 -0500

    Enhanced Vehicle Camera config

---
+++ bin/x64/plugins/cyber_engine_tweaks/mods/EnhancedVehicleCamera/config_file.lua
@@ -0,0 +1,10 @@
+return {
+									["fov"] = 56,
+									["zMult"] = -0.20000000298023,
+									["tiltMult"] = 1.1499999761581,
+									["autoSetPerCar"] = true,
+									["sensitivity"] = 50,
+									["yMult"] = 0.89999997615814,
+									["perCarPresets"] = {
+									},
+								}
